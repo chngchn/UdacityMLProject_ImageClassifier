832c2997ac0eb63c74b34bced207cadc9df0695e
Chung Chin <chungchin.ang@gmail.com>
2022-04-13 11:26:58 +0800

    First commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Data Science Nanodegree
+## Deep Learning
+### Project: Image Classifier
+
+#### Background
+This is an Udacity project to implement an image classification application. This app will train a deep learning model on a dataset of images, then use the trained model to classify images.
+
+First, I developed my code in a Jupyter Notebook to ensure my training implementation works. Then, I converted the code to a Python application that runs from the command line.
+
+#### List of files
+- Project_Image_Classifier_Project.html: this is the html file used for the project submission.
+- 1647503900.h5: this is the trained and saved Keras model.
+- Project_Image_Classifier_Project.ipynb: this is the Jupyter Notebook where I developed my code to ensure my training implementation works.
+- label_map.json: this is the mapping file provided for the images.
+- predict.py: this is the image classifier application that runs from the command line.
+- workspace.utils: an utility file provided by the Udacity team.
---
+++ predict.py
@@ -0,0 +1,68 @@
+import sys
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from PIL import Image
+import argparse
+import json
+
+batch_size = 32
+image_size = 224
+
+classes = {}
+
+def process_image(image):
+    image = tf.convert_to_tensor(image, dtype = tf.float32)
+    image = tf.image.resize(image, (image_size, image_size))
+    image /= 255
+    return image.numpy()
+
+def predict(image_path, model, top_k = 5):
+    image = Image.open(image_path)
+    image = np.asarray(image)
+    image = process_image(image)
+    expanded_image = np.expand_dims(image, axis = 0)   
+    preds = model.predict(expanded_image)
+    
+    probs = -np.partition(-preds[0], top_k)[:top_k]
+    classes = np.argpartition(-preds[0], top_k)[:top_k]
+    
+    return probs, classes
+
+if __name__ == '__main__':
+    print('predict.py, running')
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arg1')
+    parser.add_argument('arg2')
+    parser.add_argument('--top_k', type = int)
+    parser.add_argument('--category_names')
+    
+    args = parser.parse_args()
+    print(args)
+    
+    print('arg1:', args.arg1)
+    print('arg2:', args.arg2)
+    print('top_k:', args.top_k)
+    print('category_names', args.category_names)
+    
+    image_path = args.arg1
+    
+    model = tf.keras.models.load_model(args.arg2, custom_objects = {'KerasLayer': hub.KerasLayer}, compile = False)
+    top_k = args.top_k
+    if top_k is None:
+        top_k = 5
+        
+    probs, classes = predict(image_path, model, top_k)
+    
+    if args.category_names != None:
+        with open(args.category_names, 'r') as f:
+            class_names = json.load(f)
+        keys = [str(x+1) for x in list(classes)]
+        classes = [class_names.get(key) for key in keys]
+    
+    print('Probabilities: ', probs)
+    print('Classes: ', classes)
